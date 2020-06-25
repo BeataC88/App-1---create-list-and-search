2bfb310c2997d82de956100c46a838f7b328583e
Beata Czukiewska <czukiewska.b@gmail.com>
2020-06-25 19:23:00 +0200

    add random button and function

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 const form = document.querySelector('form');
 const inputToEnter = document.querySelector('.add input');
 const inputToSearch = document.querySelector('input.search');
-const btnRemove = document.querySelector('.remove');
+const btnRemove = document.querySelector('button.remove');
 const teamNumber = document.querySelector('span');
 const ul = document.querySelector('ul');
 const liList = document.getElementsByClassName('team');
 const teamArr = [];
+const btnRandom = document.querySelector('button.random');
+const teamsToRandom = [];
+let number = 0;
+const championName = document.createElement('div');
+
+const randomChampion = () => {
+    championName.textContent = "";
+    const indexToRandom = Math.floor(Math.random() * teamsToRandom.length);
+    document.body.appendChild(championName);
+    championName.style.fontSize = '30px';
+    championName.style.fontWeight = '700';
+    championName.textContent = `And the Champion is ${teamsToRandom[indexToRandom]}`;
+}
 
 const removeAllTeams = () => {
     teamArr.length = 0;
     ul.textContent = "";
     teamNumber.textContent = liList.length;
+    championName.textContent = "";
 }
 
 const removeOneTeam = (e) => {
@@ -24,18 +38,22 @@ const removeOneTeam = (e) => {
 
 const addTeam = (e) => {
     e.preventDefault();
+    number++;
     const teamName = inputToEnter.value.toUpperCase();
     if (teamName === "") return;
     const team = document.createElement('li');
     team.className = "team";
-    team.innerHTML = teamName + `<button class="deleteTeam">X</button>`;
+    team.style.listStyleType = "none";
+    team.innerHTML = `${number}.${teamName}` + `<button class = "deleteTeam"> X </button>`;
     teamArr.push(team);
+    teamsToRandom.push(teamName);
     // console.log(teamArr);
     refreshList();
     ul.appendChild(team);
     inputToEnter.value = "";
     teamNumber.textContent = liList.length;
     team.querySelector('.deleteTeam').addEventListener('click', removeOneTeam);
+    btnRandom.addEventListener('click', randomChampion);
 }
 
 const refreshList = () => {
@@ -55,6 +73,8 @@ const searchTeam = (e) => {
     teams.forEach(li => ul.appendChild(li));
     teamNumber.textContent = teams.length;
 }
+
+
 
 
 btnRemove.addEventListener('click', removeAllTeams);
